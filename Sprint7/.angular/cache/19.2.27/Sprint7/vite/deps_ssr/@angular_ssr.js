82dbaf461aca8500c6824ac4f6bc528095faad1b
Jonatas <jonatasargologouveia@gmail.com>
2026-06-16 11:49:57 -0300

    alterações no login por enquanto

---
+++ Sprint7/.angular/cache/19.2.27/Sprint7/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,38 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-2ESUA6V3.js";
+import "./chunk-EUEAV7Y5.js";
+import "./chunk-XMSFGTG5.js";
+import "./chunk-BQCK6CQ3.js";
+import "./chunk-WNXFQNHD.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
